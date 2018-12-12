7d55adf41b4984107cd3a2f64e39b334eb5779b1
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2018-12-12 10:54:14 +0800

    Remove knn bpr

--- model/knn.go
+++ model/knn.go
@@ -22,10 +22,6 @@ type KNN struct {
 	simMetric    Similarity
 	k            int
 	minK         int
-	target       string
-	lr           float64
-	reg          float64
-	nEpochs      int
 	shrinkage    int
 }
 
@@ -50,28 +46,12 @@ func (knn *KNN) SetParams(params Params) {
 	knn.userBased = knn.Params.GetBool(UserBased, true)
 	knn.k = knn.Params.GetInt(K, 40)
 	knn.minK = knn.Params.GetInt(MinK, 1)
-	knn.target = knn.Params.GetString(Target, Regression)
-	knn.lr = knn.Params.GetFloat64(Lr, 0.005)
-	knn.reg = knn.Params.GetFloat64(Reg, 0.02)
-	knn.nEpochs = knn.Params.GetInt(NEpochs, 20)
 	knn.shrinkage = knn.Params.GetInt(Shrinkage, 100)
 }
 
 func (knn *KNN) Predict(userId, itemId int) float64 {
 	denseUserId := knn.UserIdSet.ToDenseId(userId)
 	denseItemId := knn.ItemIdSet.ToDenseId(itemId)
-	// Select predict function
-	switch knn.target {
-	case Regression:
-		return knn.predictRegression(denseUserId, denseItemId)
-	case BPR:
-		return knn.predictBPR(denseUserId, denseItemId)
-	default:
-		panic("Unknown target")
-	}
-}
-
-func (knn *KNN) predictRegression(denseUserId, denseItemId int) float64 {
 	// Set user based or item based
 	var leftId, rightId int
 	if knn.userBased {
@@ -118,29 +98,9 @@ func (knn *KNN) predictRegression(denseUserId, denseItemId int) float64 {
 	return prediction
 }
 
-func (knn *KNN) predictBPR(denseUserId, denseItemId int) float64 {
-	prediction := 0.0
-	knn.UserRatings[denseUserId].ForEach(func(i, index int, value float64) {
-		if index != denseItemId {
-			prediction += knn.SimMatrix[denseItemId][index]
-		}
-	})
-	return prediction
-}
-
 // Fit a KNN model.
 func (knn *KNN) Fit(trainSet TrainSet, options ...FitOption) {
 	knn.Init(trainSet, options)
-	// Select fit function
-	switch knn.target {
-	case Regression:
-		knn.fitRegression(trainSet)
-	case BPR:
-		knn.fitBPR(trainSet)
-	}
-}
-
-func (knn *KNN) fitRegression(trainSet TrainSet) {
 	// Set global GlobalMean for new users (items)
 	knn.GlobalMean = trainSet.GlobalMean
 	// Retrieve user (item) iRatings
@@ -195,53 +155,4 @@ func (knn *KNN) fitRegression(trainSet TrainSet) {
 			}
 		}
 	})
-}
-
-func (knn *KNN) fitBPR(trainSet TrainSet) {
-	knn.UserRatings = trainSet.UserRatings
-	// Create the set of positive feedback
-	positiveSet := make([]map[int]bool, trainSet.UserCount())
-	for denseUserId, userRating := range trainSet.UserRatings {
-		positiveSet[denseUserId] = make(map[int]bool)
-		userRating.ForEach(func(i, index int, value float64) {
-			positiveSet[denseUserId][index] = true
-		})
-	}
-	// Training
-	for epoch := 0; epoch < knn.nEpochs; epoch++ {
-		// Generate permutation
-		perm := knn.rng.Perm(trainSet.Len())
-		// Training epoch
-		for _, i := range perm {
-			// Select a positive sample
-			denseUserId, densePosId, _ := trainSet.GetDense(i)
-			// Select a negative sample
-			denseNegId := -1
-			for {
-				temp := knn.rng.Intn(trainSet.ItemCount())
-				if _, exist := positiveSet[denseUserId][temp]; !exist {
-					denseNegId = temp
-					break
-				}
-			}
-			diff := knn.predictBPR(denseUserId, densePosId) - knn.predictBPR(denseUserId, denseNegId)
-			grad := math.Exp(-diff) / (1.0 + math.Exp(-diff))
-			// Update positive weights
-			knn.UserRatings[denseUserId].ForEach(func(i, index int, value float64) {
-				if index != densePosId {
-					// TODO: Add regularization
-					knn.SimMatrix[index][densePosId] += knn.lr * grad
-					knn.SimMatrix[densePosId][index] += knn.lr * grad
-				}
-			})
-			// Update negative weights
-			knn.UserRatings[denseUserId].ForEach(func(i, index int, value float64) {
-				if index != denseNegId {
-					// TODO: Add regularization
-					knn.SimMatrix[index][denseNegId] -= knn.lr * grad
-					knn.SimMatrix[denseNegId][index] -= knn.lr * grad
-				}
-			})
-		}
-	}
 }
