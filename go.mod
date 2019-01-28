8828803665789948d5f4a148af72f6d11ff787a6
sine_x <zhangzhenghao@hotmail.com>
2019-01-28 15:05:37 +0800

    Create gorse server

--- go.mod
+++ go.mod
@@ -2,14 +2,17 @@ module github.com/zhenghaoz/gorse
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/emicklei/go-restful v2.8.1+incompatible
 	github.com/fatih/color v1.7.0 // indirect
+	github.com/go-sql-driver/mysql v1.4.1
 	github.com/mattn/go-colorable v0.0.9 // indirect
 	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/mattn/go-runewidth v0.0.3 // indirect
 	github.com/olekukonko/tablewriter v0.0.1
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.2.2
-	golang.org/x/sys v0.0.0-20190109145017-48ac38b7c8cb // indirect
+	golang.org/x/sys v0.0.0-20190109145017-48ac38b7c8cb
 	gonum.org/v1/gonum v0.0.0-20181221232945-d801be0689e7
+	gopkg.in/DATA-DOG/go-sqlmock.v1 v1.3.0
 	gopkg.in/cheggaaa/pb.v1 v1.0.27
 )
---
+++ server/front.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"github.com/emicklei/go-restful"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func startFront() {
+	// Create a web service
+	ws := new(restful.WebService)
+	ws.Consumes(restful.MIME_JSON).Produces(restful.MIME_JSON)
+	// Get the recommendation list
+	ws.Route(ws.GET("/top/{user-id}").
+		To(getTop).
+		Doc("get the top list for a user").
+		Param(ws.PathParameter("user-id", "identifier of the user").DataType("int")))
+	// Add ratings
+	ws.Route(ws.PUT("/ratings/").
+		To(putRatings).
+		Doc("put ratings"))
+	// Add ratings for a user
+	ws.Route(ws.PUT("/ratings/user/{user-id}").
+		To(putRatings).
+		Doc("put ratings"))
+	// Start web service
+	restful.DefaultContainer.Add(ws)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func getTop(request *restful.Request, response *restful.Response) {
+	// Get user ID
+	userId, err := strconv.Atoi(request.PathParameter("user-id"))
+	if err != nil {
+		logError(response.WriteError(400, err))
+		return
+	}
+	// Get the top list
+	topList, err := GetTop(db, userId)
+	if err != nil {
+		logError(response.WriteError(500, err))
+		return
+	}
+	// Send result
+	logError(response.WriteAsJson(topList))
+}
+
+func putRatings(request *restful.Request, response *restful.Response) {
+	fmt.Println(request)
+}
+
+func logError(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
