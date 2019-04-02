a77fe03f3a43070187f2724fa36869059a63c532
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2019-04-02 16:21:25 +0800

    Remove scripts

--- script/test_initialization.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo '=== RUN   Test Initialization'
-
-LOCATION=$(dirname "$0")
-
-# Build executable
-go build -o ${LOCATION}/main ${LOCATION}/../app/main.go
-
-echo '--- PASS  Test Initialization'
