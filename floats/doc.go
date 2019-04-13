d8c371193bc08a51fec9c7b8d536c0c9ba30739d
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2019-04-13 13:10:28 +0800

    docs(floats): Update package introduction

--- floats/doc.go
+++ floats/doc.go
@@ -1,13 +1,11 @@
 /*
 
-Package floats provides functions for numeric computation.
+Package floats provides functions for numeric computations.
 
-There are two versions implementation:
+There are two implementations:
 
-=> Gonum Version: functions implemented by gonum. It is the default implementation.
-
-=> AVX2 Version: functions implemented by AVX2 instructions. This implementation
-should be built with avx2 tag.
+	* gonum version: implemented by gonum. It is the default implementation.
+	* AVX2 version: implemented by AVX2 instructions. This implementation should be built with `avx2` tag.
 
 */
 package floats
