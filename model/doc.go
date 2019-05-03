06f61456cc927862cb62f6852d6bbc3bceea0055
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2019-05-03 11:54:13 +0800

    docs: change docs

--- model/doc.go
+++ model/doc.go
@@ -5,9 +5,8 @@ Package model provides models for item rating and ranking.
 There are two kinds of models: rating model and ranking model. Although rating models could be used for ranking,
 performance won't be guaranteed and even won't make sense, vice versa.
 
-=> Item rating models include: Random, Baseline, SVD(Target=Regression), SVD++, NMF, KNN, SlopeOne, CoClustering
-
-=> Item ranking models includes: ItemPop, WRMF, SVD(Target=BPR)
+	* Item rating models include: Random, Baseline, SVD(optimizer=Regression), SVD++, NMF, KNN, SlopeOne, CoClustering
+	* Item ranking models includes: ItemPop, WRMF, SVD(optimizer=BPR)
 
 */
 package model
