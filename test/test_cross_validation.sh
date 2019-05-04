5beadc3562d4f4d78f3cd53b9248a39fe58a26af
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2019-05-04 13:38:38 +0800

    test: add regression test for cross validation command

--- test/test_cross_validation.sh
+++ test/test_cross_validation.sh
@@ -5,13 +5,12 @@ echo '=== RUN   Test Cross Validation'
 
 # Configuration
 EPSILON=0.005
-LOCATION=$(dirname "$0")
 
 # Build executable
-go build -o ${LOCATION}/gorse ${LOCATION}/../cmd/gorse.go
+go install ../...
 
 # Test executable
-declare RESULT=($(${LOCATION}/gorse cv svd --load-builtin ml-100k \
+declare RESULT=($(gorse test svd --load-builtin ml-100k \
     --eval-rmse \
     --eval-mae \
     --set-n-epochs 100 \
@@ -34,9 +33,6 @@ if [[ $(echo "${RESULT[1]}-0.71508<${EPSILON}" | bc) != 1 ]]; then
     echo "--- FAIL  unexpected MAE: ${RESULT[1]} - 0.71508 >= ${EPSILON}"
     exit 1
 fi
-
-# Clear
-bash ${LOCATION}/clean_file.sh
 
 # Print success
 echo '--- PASS  Test Cross Validation'
