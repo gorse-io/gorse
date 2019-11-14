74db204f9c10d739ef21182b61a72c83dabbc2f1
sine_x <zhangzhenghao@hotmail.com>
2019-11-14 16:48:06 +0800

    change example

--- example/get_started/main.go
+++ example/get_started/main.go
@@ -9,11 +9,11 @@ import (
 
 func main() {
 	// Load dataset
-	data := core.LoadDataFromCSV("steam-100k.csv", ",", true)
+	data := core.LoadDataFromBuiltIn("ml-100k")
 	// Split dataset
 	train, test := core.Split(data, 0.2)
 	// Create model
-	svd := model.NewBPR(base.Params{
+	bpr := model.NewBPR(base.Params{
 		base.NFactors:   10,
 		base.Reg:        0.01,
 		base.Lr:         0.05,
@@ -22,12 +22,18 @@ func main() {
 		base.InitStdDev: 0.001,
 	})
 	// Fit model
-	svd.Fit(train, nil)
+	bpr.Fit(train, nil)
 	// Evaluate model
-	scores := core.EvaluateRank(svd, test, train, 10, core.Precision, core.Recall, core.NDCG)
+	scores := core.EvaluateRank(bpr, test, train, 10, core.Precision, core.Recall, core.NDCG)
 	fmt.Printf("Precision@10 = %.5f\n", scores[0])
 	fmt.Printf("Recall@10 = %.5f\n", scores[1])
 	fmt.Printf("NDCG@10 = %.5f\n", scores[1])
-	// Predict a rating
-	fmt.Printf("Predict(4,8) = %.5f\n", svd.Predict(4, 8))
+	// Generate recommendations for user(4):
+	// Get all items in the full dataset
+	items := core.Items(data)
+	// Get user(4)'s ratings in the training dataset
+	excludeItems := train.User(4)
+	// Get top 10 recommended items (excluding rated items) for user(4) using BPR
+	recommendItems, _ := core.Top(items, 4, 10, excludeItems, bpr)
+	fmt.Printf("Recommend for user(4) = %v\n", recommendItems)
 }
