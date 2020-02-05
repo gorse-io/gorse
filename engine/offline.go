5cd05816454301306bb5e33a5cdf176b4a8b818d
Raphael Horber <raphael.horber@gmail.com>
2020-02-05 11:48:29 +0100

    Return error instead of nil

--- engine/offline.go
+++ engine/offline.go
@@ -4,6 +4,7 @@ import (
 	"github.com/BurntSushi/toml"
 	"github.com/zhenghaoz/gorse/base"
 	"github.com/zhenghaoz/gorse/core"
+	"errors"
 	"log"
 )
 
@@ -86,7 +87,7 @@ func UpdateRecommends(name string, params base.Params, cacheSize int, fitJobs in
 	model := LoadModel(name, params)
 	if model == nil {
 		log.Fatalf("invalid model %v, aborting", name)
-		return nil
+		return errors.New("invalid model %v")
 	}
 	// Training model
 	log.Println("training model")
