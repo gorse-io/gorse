b515b8e745565191f6ea2496693e84ee4e6094d2
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2020-08-07 20:48:03 +0800

    fix tests

--- test/test_cross_validation.sh
+++ test/test_cross_validation.sh
@@ -11,15 +11,15 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 go install ${ROOT_DIR}/...
 
 # Test executable
-declare RESULT=($(gorse test svd --load-builtin ml-100k \
-    --eval-rmse \
-    --eval-mae \
-    --set-n-epochs 100 \
-    --set-reg 0.1 \
-    --set-lr 0.01 \
-    --set-n-factors 50 \
-    --set-init-mean 0 \
-    --set-init-std 0.001 | grep -P '\d+\.\d+(?=\()' -o))
+declare RESULT=($(gorse test als --load-builtin ml-100k \
+    --splitter k-fold \
+    --eval-precision \
+    --eval-recall \
+    --eval-ndcg \
+    --set-n-epochs 10 \
+    --set-reg 0.015 \
+    --set-n-factors 20 \
+    --n-negative 0 | grep -P '\d+\.\d+(?=\()' -o))
 
 # Check result
 if [[ -z ${RESULT[0]} || -z ${RESULT[1]} ]]; then
@@ -27,17 +27,23 @@ if [[ -z ${RESULT[0]} || -z ${RESULT[1]} ]]; then
     exit 1
 fi
 
-echo $(date +'%Y/%m/%d %H:%M:%S') RMSE = ${RESULT[0]}, MAE = ${RESULT[1]}
+echo $(date +'%Y/%m/%d %H:%M:%S') Precision@10 = ${RESULT[0]}, Recall@10 = ${RESULT[1]}, NDCG@10 = ${RESULT[2]}
 
-# Check RMSE
-if [[ $(echo "${RESULT[0]}-10.90728<${EPSILON}" | bc) != 1 ]]; then
-    echo "--- FAIL  unexpected RMSE: ${RESULT[0]} - 0.90728 >= ${EPSILON}"
+# Check precision
+if [[ $(echo "${RESULT[0]}-0.32083>-${EPSILON}" | bc) != 1 ]]; then
+    echo "--- FAIL  unexpected Precision@10: ${RESULT[0]} - 0.32083 <= -${EPSILON}"
     exit 1
 fi
 
-# Check MAE
-if [[ $(echo "${RESULT[1]}-0.71508<${EPSILON}" | bc) != 1 ]]; then
-    echo "--- FAIL  unexpected MAE: ${RESULT[1]} - 0.71508 >= ${EPSILON}"
+# Check recall
+if [[ $(echo "${RESULT[1]}-0.20906>-${EPSILON}" | bc) != 1 ]]; then
+    echo "--- FAIL  unexpected Recall@10: ${RESULT[1]} - 0.20906 <= -${EPSILON}"
+    exit 1
+fi
+
+# Check ndcg
+if [[ $(echo "${RESULT[2]}-0.37643>-${EPSILON}" | bc) != 1 ]]; then
+    echo "--- FAIL  unexpected NDCG@10: ${RESULT[2]} - 0.37643 <= -${EPSILON}"
     exit 1
 fi
 
