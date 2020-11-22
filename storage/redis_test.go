df1df227b5f1cba6ac2a1fe4d9b218e96b7651da
Somiacao <36264120+Somiacao@users.noreply.github.com>
2020-11-22 14:54:34 +0800

    redis interface implement (#62)
    * redis interface implement
    
    * redis interface review and update
    
    * redis interface review and update

--- storage/redis_test.go
+++ storage/redis_test.go
@@ -363,14 +363,14 @@ func TestRedis_DeleteItem(t *testing.T) {
 	db := newMockRedis(t)
 	defer db.Close(t)
 	// Insert ret
-	feedbacks := []Feedback{
+	feedback := []Feedback{
 		{"0", "0", 0},
 		{"1", "0", 3},
 		{"2", "0", 6},
 		{"3", "0", 9},
 		{"4", "0", 12},
 	}
-	if err := db.BatchInsertFeedback(feedbacks); err != nil {
+	if err := db.BatchInsertFeedback(feedback); err != nil {
 		t.Fatal(err)
 	}
 	// Delete item
