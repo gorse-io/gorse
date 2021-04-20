9c913ffec0f7791bbc2d48271b7f5603cc91a043
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2021-04-20 16:12:39 +0800

    feat: reduce number of configs

--- config/config_test.go
+++ config/config_test.go
@@ -14,9 +14,10 @@
 package config
 
 import (
+	"testing"
+
 	"github.com/BurntSushi/toml"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func TestLoadConfig(t *testing.T) {
@@ -25,69 +26,26 @@ func TestLoadConfig(t *testing.T) {
 
 	// database configuration
 	assert.Equal(t, "redis://localhost:6379", config.Database.CacheStore)
-	assert.Equal(t, "mysql://root@tcp(localhost:3306)/gitrec?parseTime=true", config.Database.DataStore)
+	assert.Equal(t, "mysql://root@tcp(localhost:3306)/gorse?parseTime=true", config.Database.DataStore)
 	assert.Equal(t, true, config.Database.AutoInsertUser)
 	assert.Equal(t, false, config.Database.AutoInsertItem)
-	assert.Equal(t, []string{"star"}, config.Database.MatchFeedbackType)
-	assert.Equal(t, []string{"fork"}, config.Database.RankFeedbackType)
-
-	// similar configuration
-	assert.Equal(t, 500, config.Similar.NumCache)
-	assert.Equal(t, 120, config.Similar.UpdatePeriod)
-
-	// latest configuration
-	assert.Equal(t, 500, config.Latest.NumCache)
-	assert.Equal(t, 30, config.Latest.UpdatePeriod)
-
-	// popular configuration
-	assert.Equal(t, 500, config.Popular.NumCache)
-	assert.Equal(t, 120, config.Popular.UpdatePeriod)
-	assert.Equal(t, 360, config.Popular.TimeWindow)
-
-	// cf config
-	assert.Equal(t, 1000, config.Collaborative.NumCached)
-	assert.Equal(t, "als", config.Collaborative.CFModel)
-	assert.Equal(t, 60, config.Collaborative.FitPeriod)
-	assert.Equal(t, 60, config.Collaborative.PredictPeriod)
-
-	assert.Equal(t, 0.05, config.Collaborative.Lr)
-	assert.Equal(t, 0.01, config.Collaborative.Reg)
-	assert.Equal(t, 100, config.Collaborative.NEpochs)
-	assert.Equal(t, 10, config.Collaborative.NFactors)
-	assert.Equal(t, 21, config.Collaborative.RandomState)
-	assert.Equal(t, false, config.Collaborative.UseBias)
-	assert.Equal(t, 0.0, config.Collaborative.InitMean)
-	assert.Equal(t, 0.001, config.Collaborative.InitStdDev)
-	assert.Equal(t, 1.0, config.Collaborative.Alpha)
-
-	assert.Equal(t, 10, config.Collaborative.Verbose)
-	assert.Equal(t, 100, config.Collaborative.Candidates)
-	assert.Equal(t, 10, config.Collaborative.TopK)
-	assert.Equal(t, 10000, config.Collaborative.NumTestUsers)
-
-	// rank config
-	assert.Equal(t, 60, config.Rank.FitPeriod)
-	assert.Equal(t, "r", config.Rank.Task)
-
-	assert.Equal(t, 0.05, config.Rank.Lr)
-	assert.Equal(t, 0.01, config.Rank.Reg)
-	assert.Equal(t, 100, config.Rank.NEpochs)
-	assert.Equal(t, 10, config.Rank.NFactors)
-	assert.Equal(t, 21, config.Rank.RandomState)
-	assert.Equal(t, false, config.Rank.UseBias)
-	assert.Equal(t, 0.0, config.Rank.InitMean)
-	assert.Equal(t, 0.001, config.Rank.InitStdDev)
-
-	assert.Equal(t, 10, config.Rank.Verbose)
+	assert.Equal(t, []string{"star", "fork"}, config.Database.PositiveFeedbackType)
 
 	// master configuration
 	assert.Equal(t, 8086, config.Master.Port)
 	assert.Equal(t, "127.0.0.1", config.Master.Host)
-	assert.Equal(t, 4, config.Master.Jobs)
+	assert.Equal(t, 3, config.Master.SearchJobs)
+	assert.Equal(t, 4, config.Master.FitJobs)
 	assert.Equal(t, 30, config.Master.MetaTimeout)
 
 	// server configuration
+	assert.Equal(t, 128, config.Server.DefaultN)
 	assert.Equal(t, "p@ssword", config.Server.APIKey)
+
+	// recommend configuration
+	assert.Equal(t, 12, config.Recommend.PopularWindow)
+	assert.Equal(t, 66, config.Recommend.FitPeriod)
+	assert.Equal(t, 88, config.Recommend.SearchPeriod)
 }
 
 func TestConfig_FillDefault(t *testing.T) {
