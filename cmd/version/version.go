a07c513579c6a510b309c83b5eeed21240f3489e
zhangzhenghao <33406104+zhangzhenghao@users.noreply.github.com>
2021-09-30 19:58:58 +0800

    inject build info at compile time (#278)

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -1,3 +1,24 @@
 package version
 
-const Name = "0.2.5"
+import (
+	"fmt"
+	"runtime"
+)
+
+// Default build-time variable.
+// These values are overridden via ldflags
+var (
+	Version   = "unknown-version"
+	GitCommit = "unknown-commit"
+	BuildTime = "unknown-buildtime"
+)
+
+func BuildInfo() string {
+	var buildInfo string
+	buildInfo += fmt.Sprintln("Version:\t", Version)
+	buildInfo += fmt.Sprintln("Go version:\t", runtime.Version())
+	buildInfo += fmt.Sprintln("Git commit:\t", GitCommit)
+	buildInfo += fmt.Sprintln("Built:\t\t", BuildTime)
+	buildInfo += fmt.Sprintf("OS/Arch:\t %s/%s\n", runtime.GOOS, runtime.GOARCH)
+	return buildInfo
+}
