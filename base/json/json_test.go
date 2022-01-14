9ac5ff8c36a3d6e99bc1309176a63681fcdb6138
Somiacao <36264120+Somiacao@users.noreply.github.com>
2022-01-14 22:15:08 +0800

    allow empty JSON string in relational databases (#355)

---
+++ base/json/json_test.go
@@ -0,0 +1,37 @@
+// Copyright 2022 gorse Project Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package json
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUnmarshal(t *testing.T) {
+	var a []int
+	err := Unmarshal([]byte("[1,2,3]"), &a)
+	assert.NoError(t, err)
+	assert.Equal(t, []int{1, 2, 3}, a)
+
+	err = Unmarshal([]byte(""), &a)
+	assert.NoError(t, err)
+	assert.Empty(t, a)
+}
+
+func TestMarshal(t *testing.T) {
+	data, err := Marshal(nil)
+	assert.NoError(t, err)
+	assert.Equal(t, "null", string(data))
+}
