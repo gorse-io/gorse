5c2160feee189db6981b8cffbbeb1d2b399c3a40
CyJaySong <29367599+CyJaySong@users.noreply.github.com>
2023-07-13 13:36:09 +0800

    fix(client): fix wrong data type (#725)

--- client/model.go
+++ client/model.go
@@ -39,8 +39,8 @@ type RowAffected struct {
 }
 
 type Score struct {
-	Id    string `json:"Id"`
-	Score int    `json:"Score"`
+	Id    string  `json:"Id"`
+	Score float64 `json:"Score"`
 }
 
 type User struct {
