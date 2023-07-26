7f60b80811598d89ecfeaadafefd883e27e01183
zhenghaoz <zhangzhenghao@hotmail.com>
2023-07-26 21:41:19 +0800

    add test

--- base/progress/progress.go
+++ base/progress/progress.go
@@ -13,3 +13,76 @@
 // limitations under the License.
 
 package progress
+
+import (
+	"context"
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+var spanKey = uuid.New().String()
+
+type Tracer struct {
+	spans sync.Map
+}
+
+// Start creates a root span.
+func (t *Tracer) Start(ctx context.Context, name string, total int64) (context.Context, *Span) {
+	span := &Span{name: name, total: total}
+	t.spans.Store(name, span)
+	return context.WithValue(ctx, spanKey, span), span
+}
+
+func (t *Tracer) List() []Progress {
+	var progress []Progress
+	t.spans.Range(func(key, value interface{}) bool {
+		span := value.(*Span)
+		progress = append(progress, Progress{
+			Name:  span.name,
+			Total: span.total,
+			Count: span.count,
+		})
+		return true
+	})
+	return progress
+}
+
+type Span struct {
+	name     string
+	total    int64
+	count    int64
+	err      error
+	children sync.Map
+}
+
+func (s *Span) Add(n int64) {
+	s.count += n
+}
+
+func (s *Span) End() {
+	s.count = s.total
+}
+
+func (s *Span) Error(err error) {
+	s.err = err
+}
+
+func Start(ctx context.Context, name string, total int64) (context.Context, *Span) {
+	childSpan := &Span{name: name, total: total}
+	if ctx == nil {
+		return nil, childSpan
+	}
+	span, ok := (ctx).Value(spanKey).(*Span)
+	if !ok {
+		return nil, childSpan
+	}
+	span.children.Store(name, childSpan)
+	return context.WithValue(ctx, spanKey, childSpan), childSpan
+}
+
+type Progress struct {
+	Name  string
+	Total int64
+	Count int64
+}
