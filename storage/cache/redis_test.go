a5936ef102b7b48b160fb9c8991af8db759baddd
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2023-08-19 08:29:35 +0000

    Pin redis/redis-stack version

--- storage/cache/redis_test.go
+++ storage/cache/redis_test.go
@@ -22,10 +22,10 @@ import (
 	"testing"
 	"time"
 
-	"github.com/golang/protobuf/proto"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"github.com/zhenghaoz/gorse/base/log"
+	"google.golang.org/protobuf/proto"
 )
 
 var (
