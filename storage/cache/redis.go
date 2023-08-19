0eaf09993e303482c4cf9042a34cf16023d03f0e
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2023-08-19 07:43:15 +0000

    Add error hint

--- storage/cache/redis.go
+++ storage/cache/redis.go
@@ -375,21 +375,21 @@ func (r *Redis) DeleteDocuments(ctx context.Context, collections []string, condi
 func parseSearchDocumentsResult(result any) (count int64, keys []string, documents []Document, err error) {
 	rows, ok := result.([]any)
 	if !ok {
-		return 0, nil, nil, errors.New("invalid FT.SEARCH result")
+		return 0, nil, nil, errors.Errorf("invalid FT.SEARCH result: %#v", result)
 	}
 	count, ok = rows[0].(int64)
 	if !ok {
-		return 0, nil, nil, errors.New("invalid FT.SEARCH result")
+		return 0, nil, nil, errors.Errorf("invalid FT.SEARCH result: %#v", rows[0])
 	}
 	for i := 1; i < len(rows); i += 2 {
 		key, ok := rows[i].(string)
 		if !ok {
-			return 0, nil, nil, errors.New("invalid FT.SEARCH result")
+			return 0, nil, nil, errors.Errorf("invalid FT.SEARCH result: %#v", rows[i])
 		}
 		keys = append(keys, key)
 		row, ok := rows[i+1].([]any)
 		if !ok {
-			return 0, nil, nil, errors.New("invalid FT.SEARCH result")
+			return 0, nil, nil, errors.Errorf("invalid FT.SEARCH result: %#v", rows[i+1])
 		}
 		fields := make(map[string]any)
 		for j := 0; j < len(row); j += 2 {
@@ -398,11 +398,11 @@ func parseSearchDocumentsResult(result any) (count int64, keys []string, documen
 		var document Document
 		document.Id, ok = fields["id"].(string)
 		if !ok {
-			return 0, nil, nil, errors.New("invalid FT.SEARCH result")
+			return 0, nil, nil, errors.Errorf("invalid FT.SEARCH result: %#v", fields["id"])
 		}
 		score, ok := fields["score"].(string)
 		if !ok {
-			return 0, nil, nil, errors.New("invalid FT.SEARCH result")
+			return 0, nil, nil, errors.Errorf("invalid FT.SEARCH result: %#v", fields["score"])
 		}
 		document.Score, err = strconv.ParseFloat(score, 64)
 		if err != nil {
@@ -410,7 +410,7 @@ func parseSearchDocumentsResult(result any) (count int64, keys []string, documen
 		}
 		categories, ok := fields["categories"].(string)
 		if !ok {
-			return 0, nil, nil, errors.New("invalid FT.SEARCH result")
+			return 0, nil, nil, errors.Errorf("invalid FT.SEARCH result: %#v", fields["categories"])
 		}
 		document.Categories, err = decodeCategories(categories)
 		if err != nil {
@@ -418,7 +418,7 @@ func parseSearchDocumentsResult(result any) (count int64, keys []string, documen
 		}
 		timestamp, ok := fields["timestamp"].(string)
 		if !ok {
-			return 0, nil, nil, errors.New("invalid FT.SEARCH result")
+			return 0, nil, nil, errors.Errorf("invalid FT.SEARCH result: %#v", fields["timestamp"])
 		}
 		timestampMicros, err := strconv.ParseInt(timestamp, 10, 64)
 		if err != nil {
@@ -463,21 +463,21 @@ func (r *Redis) GetTimeSeriesPoints(ctx context.Context, name string, begin, end
 func parseGetTimeSeriesPointsResult(result any) (count int64, keys []string, points []TimeSeriesPoint, err error) {
 	rows, ok := result.([]any)
 	if !ok {
-		return 0, nil, nil, errors.New("invalid FT.SEARCH result")
+		return 0, nil, nil, errors.Errorf("invalid FT.SEARCH result: %#v", result)
 	}
 	count, ok = rows[0].(int64)
 	if !ok {
-		return 0, nil, nil, errors.New("invalid FT.SEARCH result")
+		return 0, nil, nil, errors.Errorf("invalid FT.SEARCH result: %#v", rows[0])
 	}
 	for i := 1; i < len(rows); i += 2 {
 		key, ok := rows[i].(string)
 		if !ok {
-			return 0, nil, nil, errors.New("invalid FT.SEARCH result")
+			return 0, nil, nil, errors.Errorf("invalid FT.SEARCH result: %#v", rows[i])
 		}
 		keys = append(keys, key)
 		row, ok := rows[i+1].([]any)
 		if !ok {
-			return 0, nil, nil, errors.New("invalid FT.SEARCH result")
+			return 0, nil, nil, errors.Errorf("invalid FT.SEARCH result: %#v", rows[i+1])
 		}
 		fields := make(map[string]any)
 		for j := 0; j < len(row); j += 2 {
@@ -486,11 +486,11 @@ func parseGetTimeSeriesPointsResult(result any) (count int64, keys []string, poi
 		var point TimeSeriesPoint
 		point.Name, ok = fields["name"].(string)
 		if !ok {
-			return 0, nil, nil, errors.New("invalid FT.SEARCH result")
+			return 0, nil, nil, errors.Errorf("invalid FT.SEARCH result: %#v", fields["name"])
 		}
 		value, ok := fields["value"].(string)
 		if !ok {
-			return 0, nil, nil, errors.New("invalid FT.SEARCH result")
+			return 0, nil, nil, errors.Errorf("invalid FT.SEARCH result: %#v", fields["value"])
 		}
 		point.Value, err = strconv.ParseFloat(value, 64)
 		if err != nil {
@@ -498,7 +498,7 @@ func parseGetTimeSeriesPointsResult(result any) (count int64, keys []string, poi
 		}
 		timestamp, ok := fields["timestamp"].(string)
 		if !ok {
-			return 0, nil, nil, errors.New("invalid FT.SEARCH result")
+			return 0, nil, nil, errors.Errorf("invalid FT.SEARCH result: %#v", fields["timestamp"])
 		}
 		timestampMicros, err := strconv.ParseInt(timestamp, 10, 64)
 		if err != nil {
