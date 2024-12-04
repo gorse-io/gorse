057ee55c7a2ed33795451a3859a38f34219f0837
zhenghaoz <zhangzhenghao@hotmail.com>
2024-12-04 20:44:54 +0800

    implement database proxy (#895)

--- protocol/task.go
+++ protocol/task.go
@@ -20,6 +20,8 @@ import (
 	"github.com/zhenghaoz/gorse/base/progress"
 )
 
+//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative cache_store.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative data_store.proto
 //go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative protocol.proto
 
 func DecodeProgress(in *PushProgressRequest) []progress.Progress {
