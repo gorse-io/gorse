d47840156d4f412cb81c23a499c2e90864ddd1dd
zhenghaoz <zhangzhenghao@hotmail.com>
2025-05-19 22:45:18 +0800

    adam: support SIMD

--- common/nn/optimizers.go
+++ common/nn/optimizers.go
@@ -74,6 +74,8 @@ type Adam struct {
 	ms    map[*Tensor]*Tensor
 	vs    map[*Tensor]*Tensor
 	t     float32
+	b1    []float32
+	b2    []float32
 }
 
 func NewAdam(params []*Tensor, alpha float32) Optimizer {
@@ -99,16 +101,30 @@ func (a *Adam) Step() {
 		if _, ok := a.ms[p]; !ok {
 			a.ms[p] = Zeros(p.shape...)
 			a.vs[p] = Zeros(p.shape...)
+			if len(p.data) > len(a.b1) {
+				a.b1 = make([]float32, len(p.data))
+				a.b2 = make([]float32, len(p.data))
+			}
 		}
 		m, v := a.ms[p], a.vs[p]
-		for i := range p.data {
-			g := p.grad.data[i] + a.wd*p.data[i]
-			// m += (1 - beta1) * (grad - m)
-			m.data[i] += (1 - a.beta1) * (g - m.data[i])
-			// v += (1 - beta2) * (grad * grad - v)
-			v.data[i] += (1 - a.beta2) * (g*g - v.data[i])
-			// param.data -= self.lr * m / (xp.sqrt(v) + eps)
-			p.data[i] -= lr * m.data[i] / (math32.Sqrt(v.data[i]) + a.eps)
-		}
+		b1, b2 := a.b1[:len(p.data)], a.b2[:len(p.data)]
+
+		// grad = grad + wd * param.data
+		floats.MulConstTo(p.data, a.wd, b1)
+		floats.Add(b1, p.grad.data)
+		// m += (1 - beta1) * (grad - m)
+		floats.SubTo(b1, m.data, b2)
+		floats.MulConstAddTo(b2, 1-a.beta1, m.data)
+		// v += (1 - beta2) * (grad * grad - v)
+		floats.MulTo(b1, b1, b2)
+		floats.Sub(b2, v.data)
+		floats.MulConstAddTo(b2, 1-a.beta2, v.data)
+		// param.data -= self.lr * m / (xp.sqrt(v) + eps)
+		copy(b2, v.data)
+		floats.Sqrt(b2)
+		floats.AddConst(b2, a.eps)
+		copy(b1, m.data)
+		floats.Div(b1, b2)
+		floats.MulConstAddTo(b1, -lr, p.data)
 	}
 }
