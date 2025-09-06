5b3769340df2deab72e2164519a40488ed77189a
zhenghaoz <zhangzhenghao@hotmail.com>
2025-09-06 17:20:57 +0800

    Fix user-to-user recommendation preview (#1054)

--- master/rest.go
+++ master/rest.go
@@ -834,13 +834,13 @@ func (m *Master) getRecommend(request *restful.Request, response *restful.Respon
 	}
 
 	// Send result
-	details := make([]data.Item, len(results))
+	details := make([]data.Item, 0, len(results))
 	for i := range results {
-		var exist bool
-		details[i], exist = itemsMap[results[i]]
-		if !exist {
-			server.InternalServerError(response, fmt.Errorf("item `%s` not found", results[i]))
-			return
+		detail, exist := itemsMap[results[i]]
+		if exist {
+			details = append(details, detail)
+		} else {
+			log.Logger().Warn("recommended item doesn't exist", zap.String("item_id", results[i]))
 		}
 	}
 	server.Ok(response, details)
