b8ffabd7c30ca9b52c1178ecd3ed34ae17ece28d
zhenghaoz <zhangzhenghao@hotmail.com>
2026-02-02 09:32:32 +0000

    Add OpenAI configuration environment variables

--- config/config.go
+++ config/config.go
@@ -658,6 +658,9 @@ var bindings = []configBinding{
 	{"blob.azure.account_name", "AZURE_STORAGE_ACCOUNT_NAME"},
 	{"blob.azure.account_key", "AZURE_STORAGE_ACCOUNT_KEY"},
 	{"blob.azure.connection_string", "AZURE_STORAGE_CONNECTION_STRING"},
+	{"openai.base_url", "OPENAI_BASE_URL"},
+	{"openai.auth_token", "OPENAI_AUTH_TOKEN"},
+	{"openai.chat_completion_model", "OPENAI_CHAT_COMPLETION_MODEL"},
 }
 
 // LoadConfig loads configuration from toml file.
