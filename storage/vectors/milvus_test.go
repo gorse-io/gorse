b3bd9fba1afe341e30d609f58d869656aed8d930
zhenghaoz <zhangzhenghao@hotmail.com>
2026-02-19 09:23:25 +0800

    chore: update copyright year to 2026 in vector storage files

--- storage/vectors/milvus_test.go
+++ storage/vectors/milvus_test.go
@@ -1,4 +1,4 @@
-// Copyright 2024 gorse Project Authors
+// Copyright 2026 gorse Project Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- storage/vectors/sqlite.go
+++ storage/vectors/sqlite.go
@@ -1,4 +1,4 @@
-// Copyright 2024 gorse Project Authors
+// Copyright 2026 gorse Project Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,8 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-//go:build (darwin || linux || windows) && (amd64 || arm64 || loong64 || ppc64le || riscv64 || s390x)
 
 package vectors
 
