bac96effa7f0dc9aa70bb089c2191d9cac8f4a1a
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2026-06-13 02:35:59 +0800

    fix: handle additional non-existent key error cases in GetTimeSeriesPoints function

--- storage/cache/redis.go
+++ storage/cache/redis.go
@@ -505,7 +505,8 @@ func (r *Redis) GetTimeSeriesPoints(ctx context.Context, name string, begin, end
 	result, err := r.client.TSRangeWithArgs(ctx, r.PointsTable()+":"+name, int(begin.UnixMilli()), int(end.UnixMilli()),
 		&redis.TSRangeOptions{Aggregator: redis.Last, BucketDuration: int(duration / time.Millisecond)}).Result()
 	if err != nil {
-		if strings.Contains(err.Error(), "TSDB: the key does not exist") {
+		if redis.HasErrorPrefix(err, "TSDB: the key does not exist") ||
+			redis.HasErrorPrefix(err, "key does not exist") {
 			return []TimeSeriesPoint{}, nil
 		}
 		return nil, errors.Trace(err)
