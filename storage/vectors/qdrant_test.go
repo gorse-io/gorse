6643aa4052f71decb6c04760da2fb261c45eed5e
zhenghaoz <zhangzhenghao@hotmail.com>
2026-09-21 17:06:32 +0800

    refactor(storage): inline Qdrant FP16 parameters

--- storage/vectors/qdrant_test.go
+++ storage/vectors/qdrant_test.go
@@ -19,8 +19,6 @@ import (
 	"testing"
 
 	"github.com/gorse-io/gorse/common/log"
-	"github.com/qdrant/go-client/qdrant"
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -31,11 +29,6 @@ var (
 func init() {
 	// os.Setenv("QDRANT_URI", "qdrant://127.0.0.1:6334")
 	qdrantUri = os.Getenv("QDRANT_URI")
-}
-
-func TestQdrantDenseVectorParamsUseFP16(t *testing.T) {
-	params := qdrantDenseVectorParams(4, qdrant.Distance_Cosine)
-	require.Equal(t, qdrant.Datatype_Float16, params.GetDatatype())
 }
 
 type QdrantTestSuite struct {
